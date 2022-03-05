a3eda1c416770af8e3b3a16503fc551cce0a924f
unknown <mgutierrez.hermosilla@gmail.com>
2022-03-05 02:24:56 -0300

    change project

--- backend/routes/auth.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const authController = require("../controllers/authController");
-
-router.post("/register", authController.register_user);
-
-router.post("/login", authController.login_user);
-
-module.exports = router;
